8980ff17cae664ae43c9501b3b2dcc84a517884d
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-03 22:54:17 +0430

    add label to Organization and employeeStatus forms'

--- baseinfo/forms.py
+++ baseinfo/forms.py
@@ -2,18 +2,18 @@ from django import forms
 from .models import *
 
 class EmployerForm(forms.Form):
-    title           = forms.CharField(max_length=60)
-    tel             = forms.CharField(max_length=19)
-    fax             = forms.CharField(max_length=19)
-    address         = forms.CharField(required=False)
-    insurance_code  = forms.CharField(max_length=20)
+    title           = forms.CharField(label="عنوان شرکت/سازمان:",max_length=60)
+    tel             = forms.CharField(label="تلفن تماس:",max_length=19)
+    fax             = forms.CharField(label="فکس:",max_length=19)
+    address         = forms.CharField(label="آدرس:",required=False)
+    insurance_code  = forms.CharField(label="کد بیمه:",max_length=20)
 
     def save_record(self):
         Employer.objects.create(**self.cleaned_data)
 
 class EmployeeStatusForm(forms.Form):
-    title           = forms.CharField(max_length=50)
-    description     = forms.CharField(widget=forms.Textarea)
+    title           = forms.CharField(label="عنوان وضعیت کاری:",max_length=50)
+    description     = forms.CharField(label="توضیحات:",widget=forms.Textarea)
 
     def save_record(self):
         EmployeeStatus.objects.create(**self.cleaned_data)

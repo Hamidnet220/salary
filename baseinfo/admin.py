a440c184d415f50f6d0032763096347e589bd131
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-03 08:19:53 +0430

    add models to admin site

--- baseinfo/admin.py
+++ baseinfo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Employer)
+admin.site.register(EmployeeStatus)
+admin.site.register(WorkStatus)
+admin.site.register(MaritalStatus)
+admin.site.register(Bank)
+admin.site.register(WorkGroup)
+admin.site.register(WorkPlace)
+admin.site.register(PostPlace)
+admin.site.register(Employee)
+admin.site.register(BankAccount)
+admin.site.register()

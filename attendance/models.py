99e5c8bf3b2896410fcb7ee8d9c3c4e07eb0cb89
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-06 07:24:51 +0430

    add attendacne app and crate models WorkdayStaus and Worksheet

---
+++ attendance/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from ..wage.models import Wage
+from ..baseinfo.models import Employee
+# Create your models here.
+
+class WorkdayStaus(models.Model):
+    title       = models.CharField(max_length=20)
+    description = models.TextField(blank=True,null=True)
+
+
+class Worksheet(models.Model):
+    wage            = models.ForeignKey(Wage,on_delete=models.PROTECT)
+    employee        = models.ForeignKey(Employee,on_delete=models.PROTECT)
+    date            = models.DateField()
+    work_day_stat   = models.ForeignKey(WorkdayStaus,on_delete=models.PROTECT)
+    
+

bc9c800afd0310e9629f74afdc274d3111ed116a
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-10 15:26:16 +0430

    add models to admin site

--- wage/admin.py
+++ wage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(PayType)
+admin.site.register(AdvanceAndLoan)
+admin.site.register(Wage)
+admin.site.register(WageDetail)

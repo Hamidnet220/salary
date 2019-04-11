2a9185e95be8a7d5b8822459a74e2031d6b9ccfa
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-11 09:32:29 +0430

    add __str__ func to wagestus model

--- attendance/models.py
+++ attendance/models.py
@@ -7,6 +7,9 @@ class WorkdayStaus(models.Model):
     title       = models.CharField(max_length=20)
     description = models.TextField(blank=True,null=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Worksheet(models.Model):
     wage            = models.ForeignKey(Wage,on_delete=models.PROTECT)

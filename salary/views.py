7dd1b68c7deccf2ac37b9a451a681fce4239e2ef
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-13 12:31:30 +0430

    add view.py to project

---
+++ salary/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import authenticate,login
+def login_view(request,*args,**kwargs):
+    if request.mothod=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+    user=authenticate(request,username=username,password=password)
+    if user is not None:
+        login(request,user)
+    else:
+        pass

2ea5d160c81ee22a91090c862ebd255553da9cc6
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-13 12:15:33 +0430

    add views and url for attendance app

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import *
 urlpatterns=[
-    
+    path('worksheet/',LogsheetView.as_view(),name="loghsheet"),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+import datetime
+from .models import *
+from django.views.generic import TemplateView,FormView
+from .forms import *
 # Create your views here.
+
+def logsheet_view(request,*args,**kwargs):
+    start_date=datetime.date.today()
+    end_date=datetime.timedelta(30)
+    obj=Worksheet.objects.all()
+    
+class LogsheetView(FormView):
+    form_class=AddWorksheet
+    template_name='logsheet.html'
+    success_url='/tanks/'
+    
+    def form_valid(self, form):
+        form.save_record()
+        return super().form_valid(form)
+

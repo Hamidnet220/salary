c20c775ba9dc1cdb4cd6e4eb345f6c22965843e4
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-15 16:33:34 +0430

    add delete view for employer model

--- baseinfo/views/employer_view.py
+++ baseinfo/views/employer_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
@@ -15,10 +15,26 @@ def organization_list_view(request,*args,**kwargs):
     view=ViewGenerator(Employer,{'edit_obj':['ویرایش','edit_employer'],'delete_obj':['حذف','del_employer']},True,'add_employer')
     return render(request,'list_objects.html',view.get_context_template())
 
+def delete_employer_view(request,id,*args,**kwargs):
+    obj=Employer.objects.get(id=id)
+
+    try:
+        obj.delete()
+        return redirect('employers_list')
+    except Exception as err:
+        print(err.args)
+        print(err)
+        context={
+            'message':'این شرکت اطلاعات دارای مرتبط می باشد لطفا قبل از حذف آن اطلاعات موجود را بررسی کنید.',
+            'buttons':{'back':['صفحه قبل','employers_list'],'home':['صفحه اصلی','home'],}
+        }
+        return render(request,'confirm_template.html',context)
+
+
 class AddOrganizationView(FormView):
     template_name='input_form.html'
     form_class=EmployerForm
-    success_url=reverse_lazy('employers_list ')
+    success_url=reverse_lazy('employers_list')
     
     def form_valid(self, form):
         form.save_record()

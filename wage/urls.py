cca2fcf9f65ff277c2c3b6b56affab6a6f2b5cbe
hamidnet220 <kiani.hamidreza@gmail.com>
2019-04-22 21:13:13 +0430

    add import employee,calculate wage url

--- wage/urls.py
+++ wage/urls.py
@@ -5,6 +5,10 @@ urlpatterns=[
     path('wagedetails/<int:id>',wage_detail_list_view,name='wagedetails_list'),
     path('loanlist/',loan_list_view,name='loans_list'),
     path('attendancedetails/<int:id>',attendance_detail_list_view,name='attendance_details'),
+    path('calculateworksheet/<int:id>',logsheet_detail_view,name='calculate_worksheet'),
+    path('importemployees/<int:id>',import_emoloyee_view,name='import_employees'),
+
+    
 
 ]
 
